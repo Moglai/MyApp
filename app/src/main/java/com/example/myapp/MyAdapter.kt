b3f1a44a4449c9c48c3285798d5e64044e94b5a9
Moglai <new.nechto@gmail.com>
2023-07-06 22:26:15 +1000

    Work with Adapter

--- app/src/main/java/com/example/myapp/MyAdapter.kt
+++ app/src/main/java/com/example/myapp/MyAdapter.kt
@@ -1,4 +1,30 @@
 package com.example.myapp
 
-class MyAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.task_model.view.*
+import java.nio.file.attribute.UserPrincipal
+
+class MyAdapter: RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    private var Tasklist = emptyList<ModelTask>()
+
+    class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.task_model, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.itemView.title.text = Tasklist[position].MyTask
+        holder.itemView.description.text = Tasklist[position].MyDescription
+    }
+
+    override fun getItemCount(): Int {
+        return Tasklist.size
+    }
+
 }
