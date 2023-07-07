f87c1aefb72f34d51061390be9ad35864cf4157c
Moglai <new.nechto@gmail.com>
2023-07-07 14:06:28 +1000

    Removing all unrealizeble features
    Delete all tasks and change task

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,21 +3,12 @@ package com.example.myapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.myapp.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 
 open class MainActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityMainBinding
-//    lateinit var adapter: MyAdapter
-//    lateinit var recyclerView: RecyclerView
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: NewAdapter
@@ -42,17 +33,10 @@ open class MainActivity : AppCompatActivity() {
 
         taskService.addListener(cardsListener)
 
-        val DelAlls: ImageButton = findViewById(R.id.DelAll)
         val AddAlls: ImageButton = findViewById(R.id.AddAll)
-
-
 
         AddAlls.setOnClickListener {
             val intent = Intent(this@MainActivity, MakerTask::class.java)
-            startActivity(intent)
-        }
-        DelAlls.setOnClickListener {
-            val intent = Intent(this@MainActivity, ChangeTask::class.java)
             startActivity(intent)
         }
     }
