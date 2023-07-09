b132ac37458fa9316525b82d8227d33bf890773b
Moglai <new.nechto@gmail.com>
2023-07-09 20:53:46 +1000

    Add TaskModel

---
+++ app/src/main/java/com/example/myapp/TaskModel.kt
@@ -0,0 +1,9 @@
+package com.example.myapp
+
+import java.util.UUID
+
+class TaskModel (
+    var name: String,
+    var desc: String,
+    var id: UUID = UUID.randomUUID()
+)
--- app/src/main/java/com/example/myapp/TaskViewModel.kt
+++ app/src/main/java/com/example/myapp/TaskViewModel.kt
@@ -2,9 +2,33 @@ package com.example.myapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.UUID
 
 class TaskViewModel: ViewModel() {
 
-    var name = MutableLiveData<String>()
-    var desc = MutableLiveData<String>()
+    var taskModels = MutableLiveData<MutableList<TaskModel>>()
+
+    init {
+        taskModels.value = mutableListOf()
+    }
+
+    fun addTaskModel(nnewTask: TaskModel) {
+        val list = taskModels.value
+        list!!.add(nnewTask)
+        taskModels.postValue(list)
+    }
+
+    fun updateTaskModel(id: UUID, name: String, desc: String) {
+        val list = taskModels.value
+        val taskk = list!!.find { it.id == id }!!
+        taskk.name = name
+        taskk.desc = desc
+        taskModels.postValue(list)
+    }
+
+    fun setTaskModel(taskModel: TaskModel) {
+        val list = taskModels.value
+        val taskk = list!!.find { it.id == taskModel.id }!!
+        taskModels.postValue(list)
+    }
 }
